2616f5482522f8fa2ed402d2b9355eedc8bc3063
Konstantin S. <havendv@gmail.com>
2026-04-29 05:46:42 +0400

    feat: Updated OpenAPI spec (#2)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/PDF4Dev/Generated/PDF4Dev.ComponentsClient.GetComponent.g.cs
+++ src/libs/PDF4Dev/Generated/PDF4Dev.ComponentsClient.GetComponent.g.cs
@@ -125,7 +125,7 @@ namespace PDF4Dev
                 PrepareGetComponentRequest(
                     httpClient: HttpClient,
                     httpRequestMessage: __httpRequest,
-                    id: id);
+                    id: id!);
 
                 return __httpRequest;
             }
--- src/libs/PDF4Dev/Generated/PDF4Dev.TemplatesClient.GetTemplate.g.cs
+++ src/libs/PDF4Dev/Generated/PDF4Dev.TemplatesClient.GetTemplate.g.cs
@@ -125,7 +125,7 @@ namespace PDF4Dev
                 PrepareGetTemplateRequest(
                     httpClient: HttpClient,
                     httpRequestMessage: __httpRequest,
-                    id: id);
+                    id: id!);
 
                 return __httpRequest;
             }
